fad8a219bd00371f896d65109ef09bade5df4032
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-02-17 16:41:42 +0100

    refactored libraries and the main() to work for linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
 
 #include <iostream>
-#include <conio.h> // For using _kbhit() and _ghetch()
-#include "sound.h"
+#include <termios.h> // For configuring terminal I/O behaviour (raw mode)
+#include <unistd.h> // For low-level read() function
+#include "sound.h" // Include the sound playback header
+
 
 int main() {
 	std::cout << "Press any key to play a sound. Press 'q' to quit.\n";
 
-	while(true) {
-		if(_kbhit()) { // Checks if a key is pressed
-			char ch = _getch(); // Get the keystrokes
-			
-			if (ch == 'q') break; // Exits if 'q' is pressed
 
-			playSound(); // Play a sound for each keystroke
-		}
+	// Enable raw mode to capture keystrokes instantly
+	enableRawMode();
+
+	char ch;
+	
+	// Loop where keystrokes are read
+	while(true) {
+		read(STDIN_FILENO, &ch, 1); // Reads a single character from standard input
+		if(ch == 'q') break; // Exits if 'q' is pressed
+
+		playSound(); // Plays a sound for each keystroke
 	}
+	
+	// Restore terminal settings to normal mode
+	disableRawMode();
 
 	return 0;
 }
