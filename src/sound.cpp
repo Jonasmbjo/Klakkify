8b994c1cd7bd8774b191e15cd2c79fefad626e7d
Jonas Moe Bjørnbom <jonas@bjornbom.no>
2025-02-17 16:56:15 +0100

    created sound.cpp with funciton to play a .wav file

---
+++ src/sound.cpp
@@ -0,0 +1,10 @@
+
+#include "sound.h"
+#include <cstdlib> // For system() function to play sound
+
+// Function to play a sound
+void playSound() {
+	// Use system() to play 
+
+	system("aplay beep.wav &";)
+}
